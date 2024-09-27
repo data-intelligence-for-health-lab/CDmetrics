4a0c9d1bc28533979a8a00a652f9b36c15370a17
yvesnsoga <nsogan@gmail.com>
2024-09-27 16:38:56 -0600

    update double model

--- difficulty/metrics_utils/utils.py
+++ difficulty/metrics_utils/utils.py
@@ -1,7 +1,6 @@
 from ray import air, tune
 from ray.tune.search import ConcurrencyLimiter
 from ray.tune.search.hyperopt import HyperOptSearch
-from hyperopt import hp
 from ray.tune.schedulers import AsyncHyperBandScheduler
 import itertools
 
@@ -10,33 +9,31 @@ import itertools
 def tune_parameters(model,data,target_column):
     """
     """
-    sched = AsyncHyperBandScheduler(time_attr="training_iteration", max_t=400, grace_period=20)
-    neurons = [5, 10, 15, 20]
+    neurons = [1, 1, 1, 1]
     layer_neuron_orders = [
         combination
-        for r in range(1, 4)
+        for r in range(1, 2)
         for combination in itertools.product(neurons, repeat=r)
     ]
     algo = ConcurrencyLimiter(HyperOptSearch(), max_concurrent=4)
     search_space = {
-    "learnRate": tune.choice("learnRate", [0.01, 0.03, 0.1]),
-    "batch_size": tune.choice("batch_size", [32, 64, 128]),
-    "activation": tune.choice("activation", ["relu", "tanh"]),
-    "hidden_layer_sizes": tune.choice("hidden_layer_sizes", layer_neuron_orders),
-}
+    "learnRate": tune.choice( [ 0.1]),
+    "batch_size": tune.choice( [128]),
+    "activation": tune.choice( ["relu"]),
+    "number_of_neurons": tune.choice(layer_neuron_orders),
 
+}
+    X = data.drop(target_column,axis=1)
+    Y = data[target_column]
 
     trainer_with_resources = tune.with_resources(
-            tune.with_parameters(model, data_frame=data),)
+            tune.with_parameters(model, data_x=X, data_y=Y),resources={"CPU":3})
     tuner = tune.Tuner(
             trainer_with_resources,
             param_space=search_space,
-            tune_config=tune.TuneConfig(
-                num_samples=1000,
-                search_alg=algo,
-                metric="mean_loss",
-                scheduler=sched,
-                mode="min",
+            tune_config=tune.TuneConfig( 
+                metric="mean_accuracy",
+                mode="max",
             ),
         )
     tuning_result = tuner.fit()
