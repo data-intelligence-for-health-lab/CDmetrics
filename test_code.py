6658f95fbde4f7d1b246974c41ce4c54dfbb6ec0
yvesnsoga <nsogan@gmail.com>
2024-10-07 11:02:14 -0600

    add test code

--- test_code.py
+++ test_code.py
@@ -10,11 +10,11 @@ from difficulty.metrics import utils
 
 data_bc = load_breast_cancer()
 
-bc_df = pd.DataFrame(data_bc['data'], columns=data_bc['feature_names'])
-bc_df["target"] = data_bc["target"]
+# bc_df = pd.DataFrame(data_bc['data'], columns=data_bc['feature_names'])
+# bc_df["target"] = data_bc["target"]
 
-# metrics = double_model.compute_metric(bc_df,11,"target",2,10,{"CPU":8,'GPU':1})
-metrics = prediction_uncertainty.compute_metric(bc_df,"target",100,2,10,{"CPU":8,'GPU':1})
+# # metrics = double_model.compute_metric(bc_df,11,"target",2,10,{"CPU":8,'GPU':1})
+# # metrics = prediction_uncertainty.compute_metric(bc_df,"target",100,2,10,{"CPU":8,'GPU':1})
 
 # print(utils.get_available_resources())
 
