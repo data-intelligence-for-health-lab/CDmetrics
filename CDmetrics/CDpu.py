33c8887f2038c1bb8e9a8560d2a4f44c56493733
Hyunjin Kwon <euleekwon@gmail.com>
2024-11-28 14:16:33 -0700

    Add multiprocessing

--- CDmetrics/CDpu.py
+++ CDmetrics/CDpu.py
@@ -5,11 +5,19 @@ import math
 from CDmetrics.nn import NN
 from CDmetrics.utils import tune_parameters
 from tensorflow.keras.utils import to_categorical
+import multiprocessing
+
+
+def worker(train_data_x, train_data_y, test_data_x, config):
+    model = NN(config).train(train_data_x, train_data_y)
+    prediction = model.predict(test_data_x)
+    return prediction
 
 
 def compute_metric(
     data, target_column, number_of_predictions, max_layers, max_units, resources
 ):
+    num_cpus = resources.get("CPU")
     difficulity = []
     for index in tqdm(range(len(data))):
         test_data = data.iloc[[index]]
@@ -19,7 +27,7 @@ def compute_metric(
         train_data = data.drop(index=index, axis=0)
         train_data_x = train_data.drop(columns=[target_column], axis=1)
         train_data_y = train_data[target_column].values
-        
+
         n_classes = len(set(data[target_column]))
         if n_classes > 2:
             test_data_y = to_categorical(test_data_y, num_classes=n_classes)
@@ -28,21 +36,23 @@ def compute_metric(
         best_config = tune_parameters(
             NN.tune, train_data, target_column, max_layers, max_units, resources
         )
-        best_model = NN(best_config)
+
         predictions = []
-        for i in range(number_of_predictions):
-            model = best_model.train(train_data_x, train_data_y)
-            prediction = model.predict(test_data_x)
-            predictions.append(prediction)
-        
+        with multiprocessing.Pool(processes=num_cpus) as pool:
+            async_results = [
+                pool.apply_async(
+                    worker, args=(train_data_x, train_data_y, test_data_x, best_config)
+                )
+                for _ in range(number_of_predictions)
+            ]
+            for result in async_results:
+                predictions.append(result.get())
+
         if n_classes > 2:
-            difficulity.append(
-                multi_difficulty_formula(
-                    predictions, test_data_y))
+            difficulity.append(multi_difficulty_formula(predictions, test_data_y))
 
         else:
-            difficulity.append(
-                binary_difficulty_formula(predictions, test_data_y))
+            difficulity.append(binary_difficulty_formula(predictions, test_data_y))
 
     return pd.DataFrame(difficulity)
 
@@ -60,4 +70,3 @@ def multi_difficulty_formula(predictions, y_test):
     class_difficulty = (location + distribution) / 2
     difficulty = np.mean(class_difficulty)
     return difficulty
-
